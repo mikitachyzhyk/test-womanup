c3c0e1a96f3e484306d364445e24ad125791f2e9
Mikita Chyzhyk <mikitachyzhyk@gmail.com>
2022-11-22 14:26:36 +0300

    added sorting by date

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -5,11 +5,15 @@ function TaskList({ tasks, changeTaskCompletion, changeTask, removeTask }) {
   return (
     <ul className="taskList">
       {tasks ? (
-        Array.isArray(tasks) && tasks.length === 0 ? (
+        tasks.length === 0 ? (
           'No tasks to complete.'
         ) : (
           [...tasks]
-            .reverse()
+            .sort((todo1, todo2) => {
+              return (
+                new Date(todo2.date).getTime() - new Date(todo1.date).getTime()
+              )
+            })
             .map((task) => (
               <TaskListItem
                 key={task.id}
